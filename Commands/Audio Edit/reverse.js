b3238aa74b60885f569691fdf56c9941f6866d75
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-22 22:48:37 +0300

    Update reverse.js

--- Commands/Audio Edit/reverse.js
+++ Commands/Audio Edit/reverse.js
@@ -8,7 +8,7 @@ module.exports = {
   desc: "To add reverse effect in a song",
   category: "Audio Edit",
   usage: "reverse <reply to audio>",
-  react: "🍁",
+  react: "🔉",
   start: async (Miku, m, { text, prefix, quoted, pushName, mime, body }) => {
     let media = await Miku.downloadAndSaveMediaMessage(quoted);
     let set = '-filter_complex "areverse"';
