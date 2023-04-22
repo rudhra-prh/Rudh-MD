dad71af29c97e5b3864f3dd816d3159252ca5817
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-22 22:04:31 +0300

    Update script.js

--- Commands/Core/script.js
+++ Commands/Core/script.js
@@ -8,10 +8,9 @@ module.exports = {
     react: "📑",
     category: "Core",
     start: async(Miku, m,{pushName,prefix}) => {
-        let picURL = fs.readFileSync('https://i.imgur.com/XQFNJDp.jpeg')
         let repoInfo = await axios.get('https://api.github.com/repos/princerudh/rudhra-md')
         let repo = repoInfo.data
         let txt = `         *${botName}'ꜱ ꜱᴄʀɪᴘᴛ*   \n\n *⿻*  *Total Forks:* ${repo.forks_count}\n *⿻*  *Total Stars:* ${repo.stargazers_count}\n *⿻*  *License:* ${repo.license.name}\n *⿻*  *Repo Size:* ${(repo.size/1024).toFixed(2)} MB\n *⿻*  *Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${repo.html_url}\n`
-        await Miku.sendMessage(m.from,{image:picURL, caption:txt},{quoted:m});
+        await Miku.sendMessage(m.from, {image: { url: botImage1 }, caption: textscript,}, { quoted: m });
     }
 }
