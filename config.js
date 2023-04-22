be4c212b37af1a3e444ccfac4c934fd95f8b32f8
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-22 12:11:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,51 +1,20 @@
-/* ---------------------------------------------------------------------------------/
-/                                                                                   /
-/             d8888 888    888                        888b     d888 8888888b.       /
-/            d88888 888    888                        8888b   d8888 888  "Y88b      /
-/           d88P888 888    888                        88888b.d88888 888    888      /
-/          d88P 888 888888 888  8888b.  .d8888b       888Y88888P888 888    888      /
-/         d88P  888 888    888     "88b 88K           888 Y888P 888 888    888      /
-/        d88P   888 888    888 .d888888 "Y8888b.      888  Y8P  888 888    888      /
-/       d8888888888 Y88b.  888 888  888      X88      888   "   888 888  .d88P      /
-/      d88P     888  "Y888 888 "Y888888  88888P'      888       888 8888888P"       /
-/                                                                                   / 
-/-----------------------------------------------------------------------------------/
-/ Author and Main Developer: FantoX                                                 /
-/ Github: https://github.com/FantoX001/Atlas-MD                                     /
-/ Powered By: Team ATLAS                                                            /
-/-----------------------------------------------------------------------------------/
-/             Meet Team ATLAS who holds all rights to this repository:              /
-/                                                                                   /
-/ 1. Pratyush - https://github.com/pratyush4932                                     /
-/ 2. Ahmii - https://github.com/Ahmii-kun                                           /               
-/ 3. Kai - https://github.com/Kai0071                                               /                    
-/ 4. Devime - https://github.com/Devime69                                           /
-/ 5. Jay JayOps - https://github.com/jayjay-ops                                     /
-/                                                                                   /
-/ ----------------------------------------------------------------------------------/
-/                                                                                   /
-/      With all of our hard work and defication you can enjoy this awesome bot!     /  
-/                                                                                   / 
-/----------------------------------------------------------------------------------*/
-
-
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "919895809960";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "mongodb+srv://fantox:xrto71r@cluster0.dnlowts.mongodb.net/?retryWrites=true&w=majority";
-global.sessionId = process.env.SESSION_ID || "ok";
-global.prefa = process.env.PREFIX || "-";
+global.mongodb = process.env.MONGODB || "mongodb+srv://rudhra:rudhraprh3@cluster0.wpoxqbg.mongodb.net/?retryWrites=true&w=majority";
+global.sessionId = process.env.SESSION_ID || "Rudhra3";
+global.prefa = process.env.PREFIX || ",";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas MD`;
-global.author = process.env.AUTHOR || "by: Team Atlas";
+global.packname = process.env.PACKNAME || `.Ʀ ᴜ ᴅ ʜ ʀ λ`;
+global.author = process.env.AUTHOR || "Ƥ ʀɪ ɴ ᴄ ᴇ  Ʀ ᴜ ᴅ ʜ";
 global.port = process.env.PORT || "8000";
 
 module.exports = {
@@ -55,19 +24,18 @@ module.exports = {
 // ---------------------Do Not Modify this part------------------- //
 
 global.mess = {
-  jobdone: "Job done...",
-  useradmin: "Sorry, only *Group Admins* can use this command *Baka*!",
+  jobdone: "ᴅᴏɴᴇ ✓",
+  useradmin: "ꜱᴏʀʀʏ, ᴏɴʟʏ *ɢʀᴏᴜᴘ ᴀᴅᴍɪɴꜱ* ᴄᴀɴ ᴜꜱᴇ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ !",
   botadmin:
-    "Sorry, i cant execute this command without being an *Admin* of this group.",
-  botowner: "Only my *Owner* can use this command, Baka!",
-  grouponly: "This command is only made for *Groups*, Baka!",
-  privateonly: "This command is only made for *Private Chat*, Baka!",
-  botonly: "Only the *Bot itself* can use this command!",
-  waiting: "Chotto Matte...",
-  nolink: "Please provide me *link*, Baka!",
-  error: "An error occurd!",
-  banned: `You are *Banned* fron using commands!  \n\nType *${prefa}owner* or *${prefa}support* to submit a request to unban yourself !`,
-  bangc: "This Group is *Banned* from using Commands!",
-  nonsfw: "Dont be a pervert Baka! This is not a NSFW enabled group!",
+    "ꜱᴏʀʀʏ, ɪ ᴄᴀɴᴛ ᴇxᴇᴄᴜᴛᴇ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ ᴡɪᴛʜᴏᴜᴛ ʙᴇɪɴɢ ᴀɴ *ᴀᴅᴍɪɴ* ᴏꜰ ᴛʜɪꜱ ɢʀᴏᴜᴘ.",
+  botowner: "ᴏɴʟʏ ᴍʏ *ᴏᴡɴᴇʀ* ᴄᴀɴ ᴜꜱᴇ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ !",
+  grouponly: "ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ ɪꜱ ᴏɴʟʏ ᴍᴀᴅᴇ ꜰᴏʀ *ɢʀᴏᴜᴘꜱ* !",
+  privateonly: "ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ ɪꜱ ᴏɴʟʏ ᴍᴀᴅᴇ ꜰᴏʀ *ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ* !",
+  botonly: "ᴏɴʟʏ ᴛʜᴇ *ʙᴏᴛ ɪᴛꜱᴇʟꜰ* ᴄᴀɴ ᴜꜱᴇ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ !",
+  waiting: "ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ᴀ ᴍᴏᴍᴇɴᴛ...",
+  nolink: "ᴘʟᴇᴀꜱᴇ ɢɪᴠᴇ ᴍᴇ ᴛʜᴇ *ʟɪɴᴋ*, !",
+  error: "ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀᴅ!",
+  banned: `ʏᴏᴜ ᴀʀᴇ *ʙᴀɴɴᴇᴅ* ꜰʀᴏᴍ ᴜꜱɪɴɢ ᴄᴏᴍᴍᴀɴᴅꜱ!  \n\nᴛʏᴘᴇ *${prefa}ᴏᴡɴᴇʀ* ᴏʀ *${prefa}ꜱᴜᴘᴘᴏʀᴛ* ᴛᴏ ꜱᴜʙᴍɪᴛ ᴀ ʀᴇQᴜᴇꜱᴛ ᴛᴏ ᴜɴʙᴀɴ ʏᴏᴜʀꜱᴇʟꜰ !`,
+  bangc: "ᴛʜɪꜱ ɢʀᴏᴜᴘ ɪꜱ *ʙᴀɴɴᴇᴅ* ꜰʀᴏᴍ ᴜꜱɪɴɢ ᴄᴏᴍᴍᴀɴᴅꜱ!",
+  nonsfw: "ᴅᴏɴᴛ ʙᴇ ᴀ ᴘᴇʀᴠᴇʀᴛ ᴛʜɪꜱ ɪꜱ ɴᴏᴛ ᴀ *ɴꜱꜰᴡ* ᴇɴᴀʙʟᴇᴅ ɢʀᴏᴜᴘ!",
 };
-
