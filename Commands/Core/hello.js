fab51bd7590a9efab1402f0e9b39e15a96c81801
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-24 00:58:16 +0300

    Update hello.js

--- Commands/Core/hello.js
+++ Commands/Core/hello.js
@@ -5,6 +5,14 @@ module.exports = {
     react: "👋",
     category: "Core",
     start: async(Miku, m,{pushName,prefix}) => {
+        const pad = (s) => (s < 10 ? "0" : "") + s;
+        const formatTime = (seconds) => {
+        const hours = Math.floor(seconds / (60 * 60));
+        const minutes = Math.floor((seconds % (60 * 60)) / 60);
+        const secs = Math.floor(seconds % 60);
+        return time = `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
+        };
+        const uptime = () => formatTime(process.uptime());
         m.reply(`\n        *ʜᴇʏ👋* *${pushName}* \n    ɪ ᴀᴍ *${botName}* ʙᴏᴛ.\n\n*⿻*   _Server Uptime:_*${uptime()}*\n*⿻*   _Status:_ *Operational*\n╭─────────────────»\n│        *ɪꜰ ʏᴏᴜ ɴᴇᴇᴅ ʜᴇʟᴘ*\n│       *ᴛʏᴘᴇ* 💬 *${prefix}help* *${prefix}menu*\n│               *ᴛʜᴀɴᴋ ʏᴏᴜ*\n╰─────────────────»\n`)
         let buttons = [
         {buttonId: `owner`, buttonText: {displayText: 'Owner'}, type: 1}
