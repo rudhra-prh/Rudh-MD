44d873625dc8a1f38928d7cbdae284fd7e706e64
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-23 04:28:13 +0300

    Update texttospeech.js

--- Commands/Essentials/texttospeech.js
+++ Commands/Essentials/texttospeech.js
@@ -24,7 +24,7 @@ module.exports = {
 
     Miku.sendMessage(
       m.from,
-      { audio: { url: texttospeechurl }, mimetype: "audio/ogg; codecs=opus", ptt: true" },
+      { audio: { url: texttospeechurl }, mimetype: "audio/codecs=opus" },
       { quoted: m }
     ).catch((e) => {
       m.reply(`An error Occurd !`);
