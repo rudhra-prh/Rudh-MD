e4f8cd55f2d626bbfa422a79d580e08317d428a4
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-24 00:18:18 +0300

    Update hello.js

--- Commands/Core/hello.js
+++ Commands/Core/hello.js
@@ -5,18 +5,26 @@ module.exports = {
     react: "👋",
     category: "Core",
     start: async(Miku, m,{pushName,prefix}) => {
-        const pad = (s) => (s < 10 ? "0" : "") + s;
-        const formatTime = (seconds) => {
-        const hours = Math.floor(seconds / (60 * 60));
-        const minutes = Math.floor((seconds % (60 * 60)) / 60);
-        const secs = Math.floor(seconds % 60);
-        return time = `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
-        };
-        const uptime = () => formatTime(process.uptime());
-        await Miku.sendMessage(m.from,{image:{url:botImage1},
-        caption:`\n        *ʜᴇʏ👋* *${pushName}* \n    ɪ ᴀᴍ *${botName}* ʙᴏᴛ.\n\n*⿻*   _Server Uptime:_*${uptime()}*\n*⿻*   _Status:_ *Operational*\n╭─────────────────»\n│        *ɪꜰ ʏᴏᴜ ɴᴇᴇᴅ ʜᴇʟᴘ*\n│       *ᴛʏᴘᴇ* 💬 *${prefix}help* *${prefix}menu*\n│               *ᴛʜᴀɴᴋ ʏᴏᴜ*\n╰─────────────────»\n`,
+        m.reply(`\n        *ʜᴇʏ👋* *${pushName}* \n    ɪ ᴀᴍ *${botName}* ʙᴏᴛ.\n\n*⿻*   _Server Uptime:_*${uptime()}*\n*⿻*   _Status:_ *Operational*\n╭─────────────────»\n│        *ɪꜰ ʏᴏᴜ ɴᴇᴇᴅ ʜᴇʟᴘ*\n│       *ᴛʏᴘᴇ* 💬 *${prefix}help* *${prefix}menu*\n│               *ᴛʜᴀɴᴋ ʏᴏᴜ*\n╰─────────────────»\n`)
+        let buttons = [
+        {buttonId: `owner`, buttonText: {displayText: 'Owner'}, type: 1}
+        ]
+        let buttonMessage = {
+        image: {url:botImage1},
+        jpegThumbnail: log0,
+        caption: teks,
         footer: `${botName}`,
-        )
-        {quoted:m})
-    }
+        buttons: buttons,
+        headerType: 4,
+        contextInfo:{externalAdReply:{
+        title:"I deserve something for my hardwork",
+        body: "Click to donate", 
+        thumbnail: fs.readFileSync("Media/Img/BG.jpg"),
+        mediaType:1,
+        mediaUrl: 'https://i.imgur.com/XQFNJDp.jpeg',
+        sourceUrl: "https://instagram.com/princerudh"
+        }}
+        }
+        await Miku.sendMessage(m.from, buttonMessage, { quoted: m })
+            }
 }
