b4fae3eec59ca5da7b12da6163804abe9ddd92a6
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-24 14:53:19 +0300

    Update hello.js

--- Commands/Core/hello.js
+++ Commands/Core/hello.js
@@ -13,26 +13,16 @@ module.exports = {
         return time = `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
         };
         const uptime = () => formatTime(process.uptime());
-        m.reply( `\n        *ʜᴇʏ👋* *${pushName}* \n    ɪ ᴀᴍ *${botName}* ʙᴏᴛ.\n\n*⿻*   _Server Uptime:_*${uptime()}*\n*⿻*   _Status:_ *Operational*\n╭─────────────────»\n│        *ɪꜰ ʏᴏᴜ ɴᴇᴇᴅ ʜᴇʟᴘ*\n│       *ᴛʏᴘᴇ* 💬 *${prefix}help* *${prefix}menu*\n│               *ᴛʜᴀɴᴋ ʏᴏᴜ*\n╰─────────────────»\n`)
         const buttons = [
-        {buttonId: `${prefix}owner`, buttonText: {displayText: 'ᴍᴇɴᴜ'}, type: 1}
-        ]
+                    {buttonId: `${prefix}help}`, buttonText: {displayText: 'ᴍᴇɴᴜ' }, type: 2}
+                ]
         const buttonMessage = {
-        image: {url:botImage1},
-        jpegThumbnail: log0,
-        caption: teks,
-        footer: `${botName}`,
-        buttons: buttons,
-        headerType: 4,
-        contextInfo:{externalAdReply:{
-        title:"Rudhra WhatsApp BOT",
-        body: "Follow me", 
-        thumbnail: fs.readFileSync("Media/Img/rudhthumb.png"),
-        mediaType:1,
-        mediaUrl: 'https://i.imgur.com/XQFNJDp.jpeg',
-        sourceUrl: "https://instagram.com/princerudh"
-        }}
-        }
+                    image: { url: anu.thumbnail },
+                    caption: `\n        *ʜᴇʏ👋* *${pushName}* \n    ɪ ᴀᴍ *${botName}* ʙᴏᴛ.\n\n*⿻*   _Server Uptime:_*${uptime()}*\n*⿻*   _Status:_ *Operational*\n╭─────────────────»\n│        *ɪꜰ ʏᴏᴜ ɴᴇᴇᴅ ʜᴇʟᴘ*\n│       *ᴛʏᴘᴇ* 💬 *${prefix}help* *${prefix}menu*\n│               *ᴛʜᴀɴᴋ ʏᴏᴜ*\n╰─────────────────»\n`,
+                    footer: `${botName}`,
+                    buttons: buttons,
+                    headerType: 4
+                }
         await Miku.sendMessage(m.from, buttonMessage, { quoted: m })
-            }
+    }
 }
