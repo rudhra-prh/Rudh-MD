d8e66cb9d4b921d92aafc6a790fe5f38882213c7
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:20:20 +0300

    Update chocolate.js

--- Commands/Logo Maker/chocolate.js
+++ Commands/Logo Maker/chocolate.js
@@ -4,12 +4,12 @@ module.exports = {
     name: "chocolate",
     alias: ["choco"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}chocolate Atlas Bot*`);
         maker.textpro("https://textpro.me/chocolate-cake-text-effect-890.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
