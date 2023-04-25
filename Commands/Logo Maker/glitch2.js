f8e997faa85325dadb2e378b667173b8061a13e4
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:29:17 +0300

    Update glitch2.js

--- Commands/Logo Maker/glitch2.js
+++ Commands/Logo Maker/glitch2.js
@@ -4,14 +4,14 @@ module.exports = {
     name: "glitch2",
     alias: ["g2"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text.includes("|")) return m.reply(`Example: *${prefix}glitch2 Atlas Bot | Team Atlas*`);
         teks1 = text.split("|")[0]
         teks2 = text.split("|")[1]
         maker.textpro("https://textpro.me/create-a-glitch-text-effect-online-free-1026.html", [
-    `${teks1}`,`${teks2}`]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${teks1}`,`${teks2}`]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
