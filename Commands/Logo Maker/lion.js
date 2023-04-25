3a1a92f06cbdadcb49b6cdc149906f228fc8fdcb
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:32:35 +0300

    Update lion.js

--- Commands/Logo Maker/lion.js
+++ Commands/Logo Maker/lion.js
@@ -4,14 +4,14 @@ module.exports = {
     name: "lion",
     alias: ["lionstyle"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text.includes("|")) return m.reply(`Example: *${prefix}lion Atlas Bot | Team Atlas*`);
         teks1 = text.split("|")[0]
         teks2 = text.split("|")[1]
         maker.textpro("https://textpro.me/create-lion-logo-mascot-online-938.html", [
-    `${teks1}`,`${teks2}`]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${teks1}`,`${teks2}`]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
