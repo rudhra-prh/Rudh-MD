6e6ffa11e91c744be3d2f1381e1181803f3b5180
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:55:28 +0300

    Update wall.js

--- Commands/Logo Maker/wall.js
+++ Commands/Logo Maker/wall.js
@@ -4,12 +4,12 @@ module.exports = {
     name: "wall",
     alias: ["thewall","wallstyle"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}wall Atlas Bot*`);
         maker.textpro("https://textpro.me/break-wall-text-effect-871.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
