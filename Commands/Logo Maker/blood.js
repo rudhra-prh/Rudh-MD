8c0674971aa3306028c0c21f83574a6f86d211a0
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:17:34 +0300

    Update blood.js

--- Commands/Logo Maker/blood.js
+++ Commands/Logo Maker/blood.js
@@ -4,12 +4,12 @@ module.exports = {
     name: "blood",
     alias: ["bld"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}blood Atlas Bot*`);
         maker.textpro("https://textpro.me/horror-blood-text-effect-online-883.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
