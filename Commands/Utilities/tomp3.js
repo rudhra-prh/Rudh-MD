8a23044ed9d65c6a18a62d73f18bfb90b0a8048a
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-25 08:56:49 +0300

    Update tomp3.js

--- Commands/Utilities/tomp3.js
+++ Commands/Utilities/tomp3.js
@@ -6,13 +6,13 @@ module.exports = {
     desc: "To get .mp3 file from a voice note or video",
     category: "Utilities",
     usage: "tomp3 <reply to audio/video>",
-    react: "🍁",
+    react: "🎶",
     start: async (Miku, m, { text, prefix, quoted, pushName, mime, body }) => {
         if (/document/.test(mime)) return m.reply(`Send/Reply Video/Audio You Want To Convert Into MP3 With Caption *${prefix}tomp3*`);
         if (!/video/.test(mime) && !/audio/.test(mime)) return m.reply(`Send/Reply Video/Audio You Want To Convert Into MP3 With Caption *${prefix}tomp3*`);
         if (!m.quoted) return m.reply(`Send/Reply Video/Audio You Want To Convert Into MP3 With Caption ${prefix}tomp3`);
         let media = await quoted.download()
         let audio = await toAudio(media, 'mp4')
-        Miku.sendMessage(m.from, {document: audio, mimetype: 'audio/mpeg', fileName: `Converted By ${botName} ${m.id}.mp3`}, { quoted : m })
+        Miku.sendMessage(m.from, {document: audio, mimetype: 'audio/mpeg', fileName: `ᴄᴏɴᴠᴇʀᴛᴇᴅ ʙʏ ${botName} ${m.id}.mp3`}, { quoted : m })
     }
 }
