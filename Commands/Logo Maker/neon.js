11fdcccbdc0c072811e20c233fbbb10faebd3ccf
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:46:11 +0300

    Update neon.js

--- Commands/Logo Maker/neon.js
+++ Commands/Logo Maker/neon.js
@@ -4,12 +4,12 @@ module.exports = {
     name: "neon",
     alias: ["neonstyle"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}neon Atlas Bot*`);
         maker.textpro("https://textpro.me/neon-text-effect-online-879.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
