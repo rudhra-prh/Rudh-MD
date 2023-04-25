11b1340b1f92c44ad9b5ad05cef9c9001602b217
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:52:24 +0300

    Update sparklechristmas.js

--- Commands/Logo Maker/sparklechristmas.js
+++ Commands/Logo Maker/sparklechristmas.js
@@ -4,12 +4,12 @@ module.exports = {
     name: "sparklechristmas",
     alias: ["schristmas"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}schristmas Atlas Bot*`);
         maker.textpro("https://textpro.me/sparkles-merry-christmas-text-effect-1054.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
