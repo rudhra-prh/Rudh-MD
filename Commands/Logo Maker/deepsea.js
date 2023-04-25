a742c740f772813b3a436226374181a0b0a63949
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:26:34 +0300

    Update deepsea.js

--- Commands/Logo Maker/deepsea.js
+++ Commands/Logo Maker/deepsea.js
@@ -4,12 +4,12 @@ module.exports = {
     name: "deepsea",
     alias: ["deepseastyle"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}deepsea Atlas Bot*`);
         maker.textpro("https://textpro.me/create-3d-deep-sea-metal-text-effect-online-1053.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
