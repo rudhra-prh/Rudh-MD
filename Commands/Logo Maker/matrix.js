1b962ca0bf239558003f59e706a16b8f26f76878
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:45:18 +0300

    Update matrix.js

--- Commands/Logo Maker/matrix.js
+++ Commands/Logo Maker/matrix.js
@@ -4,12 +4,12 @@ module.exports = {
     name: "matrix",
     alias: ["mtx"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}matrix Atlas Bot*`);
         maker.textpro("https://textpro.me/matrix-style-text-effect-online-884.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
