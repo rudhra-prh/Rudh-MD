eab232c437e46fb795703b956d6e8e29ca67f64e
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:30:23 +0300

    Update graffiti.js

--- Commands/Logo Maker/graffiti.js
+++ Commands/Logo Maker/graffiti.js
@@ -4,14 +4,14 @@ module.exports = {
     name: "graffiti",
     alias: ["grafiti"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text.includes("|")) return m.reply(`Example: *${prefix}graffiti Atlas Bot|Team Atlas*`);
         teks1 = text.split("|")[0]
         teks2 = text.split("|")[1]
         maker.textpro("https://textpro.me/create-a-cool-graffiti-text-on-the-wall-1010.html", [
-            `${teks1}`,`${teks2}`]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+            `${teks1}`,`${teks2}`]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
