a6bf53ed2b0d1273152ab4736f3269bbb25692b7
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:24:16 +0300

    Update circuit.js

--- Commands/Logo Maker/circuit.js
+++ Commands/Logo Maker/circuit.js
@@ -4,12 +4,12 @@ module.exports = {
     name: "circuit",
     alias: ["cir"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}circuit Atlas Bot*`);
         maker.textpro("https://textpro.me/create-blue-circuit-style-text-effect-online-1043.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
