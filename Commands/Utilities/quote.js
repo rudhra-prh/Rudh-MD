1421b451abdb6272a9828049a7d7b7094dd351cc
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-25 10:44:26 +0300

    Update quote.js

--- Commands/Utilities/quote.js
+++ Commands/Utilities/quote.js
@@ -8,7 +8,7 @@ module.exports = {
   desc: "To make any text to quote",
   category: "Search",
   usage: `q <Your line>`,
-  react: "🍁",
+  react: "🌃",
   start: async (Miku, m, { text, prefix, args, pushName }) => {
     if (!args[0] && !m.quoted)
       return m.reply(`Please provide a text (Type or mention a message) !`)
