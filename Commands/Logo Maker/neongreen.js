ebbb37429c3eaee18099956c9a2ea01b33cccac5
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:47:53 +0300

    Update neongreen.js

--- Commands/Logo Maker/neongreen.js
+++ Commands/Logo Maker/neongreen.js
@@ -4,12 +4,12 @@ module.exports = {
     name: "neongreen",
     alias: ["gereenneon","gerenneon","ngreen"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}ngreen Atlas Bot*`);
         maker.textpro("https://textpro.me/green-neon-text-effect-874.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
