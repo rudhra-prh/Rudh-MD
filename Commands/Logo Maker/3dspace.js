02890f453a98e709d3e579868afdd546ef50ccb1
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:14:02 +0300

    Update 3dspace.js

--- Commands/Logo Maker/3dspace.js
+++ Commands/Logo Maker/3dspace.js
@@ -4,14 +4,14 @@ module.exports = {
     name: "3dspace",
     alias: ["3dsp"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text.includes("|")) return m.reply(`Example: *${prefix}3dspace Atlas Bot | Team Atlas*`);
         teks1 = text.split("|")[0]
         teks2 = text.split("|")[1]
         maker.textpro("https://textpro.me/create-space-3d-text-effect-online-985.html", [
-    `${teks1}`,`${teks2}`]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${teks1}`,`${teks2}`]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
