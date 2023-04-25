99337699d2c202ffb9a6d99a00350e3b4181b68a
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:27:15 +0300

    Update demon.js

--- Commands/Logo Maker/demon.js
+++ Commands/Logo Maker/demon.js
@@ -4,12 +4,12 @@ module.exports = {
     name: "demon",
     alias: ["evil"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}demon Atlas Bot*`);
         maker.textpro("https://textpro.me/create-green-horror-style-text-effect-online-1036.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
