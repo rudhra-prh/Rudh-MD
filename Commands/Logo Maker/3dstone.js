c5a08a952829abcdb0cfd332afa875cdccf1dbde
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:14:42 +0300

    Update 3dstone.js

--- Commands/Logo Maker/3dstone.js
+++ Commands/Logo Maker/3dstone.js
@@ -4,12 +4,12 @@ module.exports = {
     name: "3dstone",
     alias: ["3ds"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}3dstone Atlas Bot*`);
         maker.textpro("https://textpro.me/3d-stone-cracked-cool-text-effect-1029.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
