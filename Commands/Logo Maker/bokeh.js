44d65ad7ad699c7cff874bb564dc28be79303292
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-04-26 02:18:07 +0300

    Update bokeh.js

--- Commands/Logo Maker/bokeh.js
+++ Commands/Logo Maker/bokeh.js
@@ -4,12 +4,12 @@ module.exports = {
     name: "bokeh",
     alias: ["bokehstyle","bokeheffect"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🛡️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}bokeh Atlas Bot*`);
         maker.textpro("https://textpro.me/bokeh-text-effect-876.html", [
-    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `ᴍᴀᴅᴇ ʙʏ ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
     }
 }
